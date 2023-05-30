349c094454d25813f26d10c8d5aa2a244673250f
edusantsouza <eduardo.santsouza@gmail.com>
2023-05-30 18:41:21 -0300

    first commit

---
+++ src/components/Home/Social/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://www.linkedin.com/in/eduardo-sant-souza/" className="home__social-icon" target='blank'>
+       <i class="uil uil-linkedin-alt"></i>
+      </a>
+      <a href="https://github.com/edusantsouza" className="home__social-icon" target='blank'>
+        <i class="uil uil-github-alt"></i>
+      </a>
+      <a href="https://medium.com/@eduardo.santsouza" className="home__social-icon" target='blank'>
+        <i class="uil uil-medium-m"></i>
+      </a>
+    </div>
+  )
+}
+
+export default Social
